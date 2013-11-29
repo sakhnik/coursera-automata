4349a8fd5a992ffb540c4a9e120c2cb391c9525a
Anatolii Sakhnik <sakhnik@gmail.com>
2013-11-29 20:57:04 +0200

    Fix PDA

--- pda.py
+++ pda.py
@@ -25,43 +25,27 @@ delta = {
     }
 
 # Initial set of states
-state = [(q,[Z0])]
+state = (q,[Z0])
 
 def print_state(s):
-    for (state, stack) in s:
-        print(state, "".join(reversed(stack)))
-    print()
-        
+    state, stack = s
+    print(state, "".join(reversed(stack)))
+
 print_state(state)
 
-# Calculate set of states after consuming a symbol with epsilon transitions.
-def process(states, symbol):
-    result = []
-    for state in states:
-        (s, st) = state
-        try:
-            new_s, new_st = delta[s, symbol, st.pop()]
-            for new_push in reversed(new_st):
-                st.append(new_push)
-            if st and (new_s, st) not in result:
-                result.append((new_s, st))
-        except KeyError:
-            pass
-        except IndexError:
-            pass
-        # Try epsilon
-        try:
-            new_s, new_st = delta[s, eps, st[-1]]
-            st2 = st[:-1]  # Create a new stack
-            for new_push in reversed(new_st):
-                st2.append(new_push)
-            if st2 and (new_s, st2) not in result:
-                result.append((new_s, st2))
-        except KeyError:
-            pass
-        except IndexError:
-            pass
-    return result
+# Calculate set of states after consuming a symbol.
+def process(state, symbol):
+    (s, st) = state
+    try:
+        new_s, new_st = delta[s, symbol, st.pop()]
+        for new_push in reversed(new_st):
+            st.append(new_push)
+        if st:
+            return (new_s, st)
+    except KeyError:
+        pass
+    except IndexError:
+        pass
 
 #task = (1,1,1,0,0,1)
 #task = (0,1,0,1,0,1,0)
@@ -69,4 +53,8 @@ def process(states, symbol):
 task = (0,1,1,0,1,1,0,1,1)
 for symb in task:
     state = process(state, symb)
+    print_state(state)
+print("---")
+while True:
+    state = process(state, eps)
     print_state(state)
